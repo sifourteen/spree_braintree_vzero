8181f934f93d799beb1e0586aacf6de05cdc9b42
Devon Anderson <devonxanderson@gmail.com>
2016-09-21 11:55:30 -0700

    Cleaning up JS to be more readable, and fixing issue where closing div wasn't being added when showing saved credit card list

--- app/assets/javascripts/spree/frontend/spree_braintree_vzero.js
+++ app/assets/javascripts/spree/frontend/spree_braintree_vzero.js
@@ -2,76 +2,113 @@
 
 SpreeBraintreeVzero = {
   updateSaveAndContinueVisibility: function() {
-    if (this.isButtonHidden()) {
-      $(this).trigger('hideSaveAndContinue')
-    } else {
-      $(this).trigger('showSaveAndContinue')
-    }
+    $(this).trigger((this.isButtonHidden() ? 'hideSaveAndContinue' : 'showSaveAndContinue'));
   },
+
   isButtonHidden: function () {
-    paymentMethod = this.checkedPaymentMethod();
+    var paymentMethod = this.checkedPaymentMethod();
+    
     return (!$('#use_existing_card_yes:checked').length && SpreeBraintreeVzero.paymentMethodID && paymentMethod.val() == SpreeBraintreeVzero.paymentMethodID);
   },
+
   checkedPaymentMethod: function() {
     return $('div[data-hook="checkout_payment_step"] input[type="radio"][name="order[payments_attributes][][payment_method_id]"]:checked');
   },
+
   hideSaveAndContinue: function() {
     this.enableSubmitButton();
-    if(!$('div[data-hook="checkout_payment_step"]').length) return
+
+    if(!$('div[data-hook="checkout_payment_step"]').length) {
+      return;
+    }
+
     $(SpreeBraintreeVzero.checkoutFormId + ' .form-buttons .btn').hide();
+
     $("[method_id='" + SpreeBraintreeVzero.paymentMethodID + "']").show();
+
     $(".new-braintree-payment-method").show();
+
     $(".show-new-payment").hide();
   },
+
   showSaveAndContinue: function() {
     this.enableSubmitButton();
-    if(!$('div[data-hook="checkout_payment_step"]').length) return
+
+    if(!$('div[data-hook="checkout_payment_step"]').length) {
+      return;
+    }
+
     $("[method_id='" + SpreeBraintreeVzero.paymentMethodID + "']").hide();
+
     $('[name="commit"]:not(.braintree-submit)').show();
+
     $(".new-braintree-payment-method").hide();
+
     $(".show-new-payment").show();
   },
+
   setSaveAndContinueVisibility: function() {
-    if($('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val())
+    if($('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val()) {
       SpreeBraintreeVzero.showSaveAndContinue();
-    else
+    }
+    else {
       SpreeBraintreeVzero.updateSaveAndContinueVisibility();
+    }
   },
+
   enableSubmitButton: function() {
-    $('.btn:disabled').attr('disabled', false).removeClass('disabled').addClass('primary');
+    $('.btn:disabled')
+      .attr('disabled', false)
+      .removeClass('disabled')
+      .addClass('primary');
   },
+
   addDeviceData: function() {
-    if(SpreeBraintreeVzero.deviceData)
+    if(SpreeBraintreeVzero.deviceData) {
       $(SpreeBraintreeVzero.checkoutFormId).append("<input type='hidden' name='device_data' value=" + SpreeBraintreeVzero.deviceData + ">");
+    }
   }
 }
 
 $(document).ready(function() {
-  paymentMethods = $('div[data-hook="checkout_payment_step"] input[type="radio"]').click(function (e) {
+  $('div[data-hook="checkout_payment_step"] input[type="radio"]').on('click', function (e) {
     SpreeBraintreeVzero.setSaveAndContinueVisibility();
   });
-  $('.saved-payment-methods').change(function (e) {
-    if($(this).val())
+
+  $('.saved-payment-methods').on('change', function (e) {
+    if($(this).val()) {
       SpreeBraintreeVzero.showSaveAndContinue();
-    else
+    }
+    else {
       SpreeBraintreeVzero.hideSaveAndContinue();
+    }
   });
-  $('#show-new-payment').click(function (e) {
+
+  $('#show-new-payment').on('click', function (e) {
     e.preventDefault();
-    $('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val('')
+
+    $('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val('');
+
     SpreeBraintreeVzero.updateSaveAndContinueVisibility();
   });
-  $('[name="commit"]:not(.braintree-submit)').click(function (e) {
+
+  $('[name="commit"]:not(.braintree-submit)').on('click', function (e) {
     if($('#checkout-step-payment').length) {
       e.preventDefault();
-      token = $('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val()
-      if(token)
+      
+      var token = $('#saved_payment_methods_for_' + SpreeBraintreeVzero.paymentMethodID).val();
+
+      if(token) {
         $(SpreeBraintreeVzero.checkoutFormId).append("<input type='hidden' name='order[payments_attributes][][braintree_token]' value=" + token + ">");
+      }
+
       $('#checkout_form_payment').submit();
     }
   });
-  $('.braintree-submit, [name="commit"]').click(function(e) {
+
+  $('.braintree-submit, [name="commit"]').on('click', function(e) {
     SpreeBraintreeVzero.addDeviceData();
   });
+
   SpreeBraintreeVzero.setSaveAndContinueVisibility();
-})
+});
